ccf8037b4db9d4749222fc70cb76f1805fa8e357
Thanh <cringeclone@gmail.com>
2025-05-21 10:44:14 +0900

    filename correction

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client'
-import App2 from './App2.tsx'
+import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(<App2 />)
+createRoot(document.getElementById('root')!).render(<App />)
