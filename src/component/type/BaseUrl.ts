70445d3bcf20eb39dc65bea18fe5635c8a118d0d
chldhntr <cringeclone@gmail.com>
2025-09-14 17:21:40 +0900

    change url

--- src/component/type/BaseUrl.ts
+++ src/component/type/BaseUrl.ts
@@ -1,7 +1,7 @@
 export class BaseUrl {
   public static returnUrl(): string {
-    //let baseUrl: string = 'https://dictionary-api-server.onrender.com'
-    let baseUrl: string = 'http://192.168.0.101:3003'
+    let baseUrl: string = 'https://dictionary-api-server.onrender.com'
+    //let baseUrl: string = 'http://192.168.0.102:3003'
     return baseUrl
   }
 }
