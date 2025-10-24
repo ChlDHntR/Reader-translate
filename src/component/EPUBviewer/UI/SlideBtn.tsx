5e8b07b2bff5031f18f0df32e5d7a20878c3d9c9
Thanh <cringeclone@gmail.com>
2025-10-24 17:30:41 +0900

    nothing burger

--- src/component/EPUBviewer/UI/SlideBtn.tsx
+++ src/component/EPUBviewer/UI/SlideBtn.tsx
@@ -1,15 +1,20 @@
 import useThemeChanger from '../../../hook/useThemeChanger'
 import { IoSunny, IoMoon } from 'react-icons/io5'
 
-export default function SlideBtn() {
+export default function SlideBtn({renditionRef} : {renditionRef: any}) {
   const { darkTheme, setDarkTheme } = useThemeChanger()
+
+  const handleThemeChange = () => {
+    if (!renditionRef.current) return
+    setDarkTheme()
+  }
 
   return (
     <div
-      className={`flex justify-around rounded-xl w-12 h-6 relative ${
+      className={`flex justify-around rounded-xl w-12 h-6 relative transition-colors ease-in delay-150 ${
         darkTheme ? 'bg-gray-400 text-gray-400' : 'bg-[#1B2B34] text-[#1B2B34]'
       }`}
-      onClick={() => setDarkTheme()}
+      onClick={handleThemeChange}
     >
       <div
         className={`absolute w-5 h-5 rounded-xl top-1/2 -translate-y-1/2 ${
