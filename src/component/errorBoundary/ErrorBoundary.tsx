4c41396e73d1906d3f3264b29661b6cab9f7e5df
Thanh <cringeclone@gmail.com>
2025-10-24 17:29:16 +0900

    Error boundary

---
+++ src/component/errorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,25 @@
+import React, { ReactNode } from "react"
+
+type ErrorBoundaryComponent = {
+    children: ReactNode,
+    fallback: ReactNode
+}
+
+export default class ErrorBoundary extends React.Component<ErrorBoundaryComponent> {
+    state = { hasError: false }
+    
+    static getDerivedStateFromError(error: Error) {
+        return { hasError: true }
+    }
+
+    componentDidCatch(error: Error, errorInfo: React.ErrorInfo): void {
+        console.log(error, errorInfo)
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return this.props.fallback
+        }
+        return this.props.children
+    }
+}
